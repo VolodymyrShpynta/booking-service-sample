7449b0d3b695f2f662846706dac9f22ea0cf24a3
Volodymyr Shpynta <vshpynta@playtika.com>
2023-07-12 16:23:29 +0300

    Create project structure.

---
+++ src/main/java/com/vshpynta/booking/service/BookingServiceSampleApplication.java
@@ -0,0 +1,13 @@
+package com.vshpynta.booking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vshpynta/booking/service/BookingServiceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vshpynta.booking.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
