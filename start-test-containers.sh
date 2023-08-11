f33f2f0161610647e1acc13fd1e7097b9be2704f
VolodymyrShpynta <vladimir.shpinta@gmail.com>
2023-08-12 00:15:54 +0300

    Add more docker containers configs.

--- start-test-containers.sh
+++ start-test-containers.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 docker-compose -f test-containers-docker-compose.yml up -d
+
+# Setup Kafka:
+sleep 5s
+# Create topics:
+docker exec booking-service-sample_kafka_1 /usr/bin/kafka-topics --create --topic booking_history --partitions 1 --replication-factor 1 --if-not-exists --bootstrap-server kafka:9092
